868f04d3a3f8ddcefda82ecbf351650bb26c8b45
Istiak-Ahmed78 <aistiak2078@gmail.com>
2026-01-08 21:08:49 +0600

    first commit

---
+++ lib/flutter_device_state_method_channel.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+
+import 'flutter_device_state_platform_interface.dart';
+
+/// An implementation of [FlutterDeviceStatePlatform] that uses method channels.
+class MethodChannelFlutterDeviceState extends FlutterDeviceStatePlatform {
+  /// The method channel used to interact with the native platform.
+  @visibleForTesting
+  final methodChannel = const MethodChannel('flutter_device_state');
+
+  @override
+  Future<String?> getPlatformVersion() async {
+    final version = await methodChannel.invokeMethod<String>('getPlatformVersion');
+    return version;
+  }
+}
