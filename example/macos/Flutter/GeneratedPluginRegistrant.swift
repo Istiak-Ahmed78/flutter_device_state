9c9f1713c6be9654a8324122acf148202805224c
Istiak-Ahmed78 <aistiak2078@gmail.com>
2026-01-09 16:08:37 +0600

    add(vpn): .\macos\ added

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_device_state
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterDeviceStatePlugin.register(with: registry.registrar(forPlugin: "FlutterDeviceStatePlugin"))
+}
